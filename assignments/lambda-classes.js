66639edb3e11cd76bebdaba16777881f4e5509a0
Vyraal1 <kevin.afable@gmail.com>
2019-08-22 14:45:17 -0400

    instructor classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,4 +1,5 @@
-// CODE here for your Lambda Classes
+//constructor takes in multiple arguments instead of a single object paramter
+
 class Person {
   constructor(name, age, location) {
     this.name = name;
@@ -11,8 +12,46 @@ class Person {
 }
 
 //Class Person Test
-const fredPerson = new Person("Fred", 28, "Bedrock");
-const bettyPerson = new Person("Betty", 25, "Miami");
-console.log(fredPerson);
-fredPerson.speak();
-bettyPerson.speak();
+const testPerson1 = new Person("Fred", 28, "Bedrock");
+const testPerson2 = new Person("Betty", 25, "Miami");
+console.log(testPerson1);
+console.log(testPerson2);
+testPerson1.speak();
+testPerson2.speak();
+
+class Instructor extends Person {
+  constructor(name, age, location, speciality, favLanguage, catchPhrase) {
+    super(name, age, location);
+    this.specialty = speciality;
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase;
+  }
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}.`);
+  }
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}.`);
+  }
+}
+
+const fred = new Instructor(
+  "Fred",
+  37,
+  "Bedrock",
+  "JavaScript",
+  "Front-end",
+  `Don't forget the homies`
+);
+const brit = new Instructor(
+  "Brit",
+  "none of our business",
+  "Can-eh-dah",
+  "JavaScript",
+  "Front-end",
+  "Please share your screen during breakouts, its mandatory."
+);
+//Instructor test cases
+console.log(fred);
+console.log(brit);
+fred.demo("how to party like a rock star");
+brit.demo("the horrors of pre-flexbox internet");
